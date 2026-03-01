a008bb2e9012cc00827e15d5a18cf56dcb0ab7bd
Serdar ŞAHİN <serdarsahinn05@gmail.com>
2026-03-01 22:23:16 +0300

    Main Updated

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,19 @@
 from fastapi import FastAPI, UploadFile, File, Response
+"""
 from fastapi.middleware.cors import CORSMiddleware
 from services.bg_remover import process_bg_removal
 from services.ocr_reader import router as ocr_router
 import uvicorn
-
+"""
 
 app = FastAPI()
-app.include_router(ocr_router)
+#app.include_router(ocr_router)
 
 @app.get("/")
 async def root():
     return {"status": "ok", "message": "Multi-Tool API is Online!"}
 
+"""
 # Çok Önemli: React (5173 portu) ve FastAPI (8000 portu) farklı yerlerde olduğu için
 # tarayıcı güvenliği (CORS) buna izin vermiyor. Bu middleware ile o engeli kaldırıyoruz.
 app.add_middleware(
@@ -33,7 +35,7 @@ async def remove_background_endpoint(file: UploadFile = File(...)):
 
     #Response: İşlenmiş resmi PNG formatında ham veri olarak geri fırlat
     return Response(content=processed_image, media_type="image/png")
-
+"""
 
 """if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
