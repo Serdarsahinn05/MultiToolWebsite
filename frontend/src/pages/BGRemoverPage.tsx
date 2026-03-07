f8dacd2777e655d903930d88e0704352994ac76a
Serdar ŞAHİN <serdarsahinn05@gmail.com>
2026-03-07 18:04:57 +0300

    bg remover updated

--- frontend/src/pages/BGRemoverPage.tsx
+++ frontend/src/pages/BGRemoverPage.tsx
@@ -55,7 +55,7 @@ const baseUrl = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000';
         />
 
         <text
-          onClick={handleUpload}
+          //onClick={handleUpload}
           //disabled={!selectedFile || loading}
           className={styles.btn}
         >
